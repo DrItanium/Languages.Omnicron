4469fd3c9d072913faa2b05146f6adb4edc02c45
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-04 23:49:19 -0800

    Updated copyright

--- Libraries/Collections/Hunk.cs
+++ Libraries/Collections/Hunk.cs
@@ -1,5 +1,5 @@
 
-//Copyright 2012 Joshua Scoggins. All rights reserved.
+//Copyright 2012-2015 Joshua Scoggins. All rights reserved.
 //
 //Redistribution and use in source and binary forms, with or without modification, are
 //permitted provided that the following conditions are met:
